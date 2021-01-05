6c440c5ed5bb36b3c44bc15adf3953ae8a6e972c
Jacob Martin <martinjacobd@gmail.com>
2021-01-05 15:18:01 -0500

    Updated some thigns

--- src/reader.lisp
+++ src/reader.lisp
@@ -1,16 +1,18 @@
 ;;; Copyright (c) Jacob Martin 2021
 
-;;; This file is part of Foobar.
+;;; This file is part of lledger.
 
-;;; Foobar is free software: you can redistribute it and/or modify
+;;; lledger is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
 ;;; the Free Software Foundation, either version 3 of the License, or
 ;;; (at your option) any later version.
 
-;;; Foobar is distributed in the hope that it will be useful,
+;;; lledger is distributed in the hope that it will be useful,
 ;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;; GNU General Public License for more details.
 
 ;;; You should have received a copy of the GNU General Public License
-;;; along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+;;; along with lledger.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; The grammar of 
